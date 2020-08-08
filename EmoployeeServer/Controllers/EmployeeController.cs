1dadb29736b8d271e01468c5a49fda9d803d1249
mina <minamedhat_fcis@hotmail.com>
2020-08-08 13:45:05 +0200

    Add project files.

---
+++ EmoployeeServer/Controllers/EmployeeController.cs
@@ -0,0 +1,61 @@
+﻿using EmoployeeServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmoployeeServer.Controllers
+{
+    [Authorize]
+    public class EmployeeController : ApiController
+    {
+        private ApplicationDbContext _context;
+        public EmployeeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET api/Employee
+        public IEnumerable<Employee> Get()
+        {
+            return _context.Employees.ToList();
+        }
+
+        // GET api/Employee/hiringDate
+        [Route("api/employee/{hiringDate}")]
+        public IHttpActionResult Get(DateTime hiringDate)
+        {
+            var date = hiringDate.Date;
+            var employees = _context.Employees.Where(e => DbFunctions.TruncateTime(e.HiringDate) == date).ToList();
+            if (employees.Count > 0)
+                return Ok(employees);
+            else
+                return NotFound();
+        }
+
+        // GET api/Employee/id
+        public IHttpActionResult Get(int id)
+        {
+            var employee = _context.Employees.SingleOrDefault(e => e.Id == id);
+            if (employee == null)
+                return NotFound();
+            return Ok(employee);
+        }
+
+        // POST api/Employee
+        [HttpPost]
+        public IHttpActionResult CreateEmployee(Employee employee)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return Created(new Uri(Request.RequestUri + "/" + employee.Id), employee);
+        }
+    }
+}
---
+++ EmoployeeServer/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace EmoployeeServer.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime HiringDate { get; set; }
+    }
+}
